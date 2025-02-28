7713563023fbb99b46f42f351d1f6766a70f87c6
joaouzeda <uzeda.joao.p@gmail.com>
2025-02-28 13:16:37 -0300

    adding whatsapp

--- Assents_tracker.ino
+++ Assents_tracker.ino
@@ -2,15 +2,18 @@
 #include "WiFi.h"
 #include "ArduinoJson.h"
 #include "HTTPClient.h"
+#include <UrlEncode.h>
 
 #define LED 35
 
 HTTPClient http;
 
-const char *ssid = "DoroteiaDiega-2.4";     // wifi name
-const char *pass = "Carnesecajerimum5106";
+const char *ssid = "your SSID";   
+const char *pass = "your wifi password";
 
 String Payload = "";
+String PhoneNumber = "Tour phone";
+String ApiKey = "your api"; //from CallMeBot
 
 void Read_Price(const String& stockname){
   String RequestAdress = "https://finnhub.io/api/v1/quote?symbol=" + stockname + "&token=cv0k499r01qo8ssh96g0cv0k499r01qo8ssh96gg";
@@ -29,12 +32,33 @@ void Read_Price(const String& stockname){
     float CurrentPrice = doc["c"];
     float DifferenceInPrice = ((CurrentPrice - ClosePrice) / ClosePrice) * 100.0;
 
-    Serial.printf("BTC price: %f \n", CurrentPrice);
-    Serial.printf("Variation: %f%% \n", DifferenceInPrice);
+    if(DifferenceInPrice > 5.0 || DifferenceInPrice < 5.0){
+      char message[32];
+      sprintf(message, "Btc price: %f \n Variation: %f%%", CurrentPrice, DifferenceInPrice);
+      Send_Message(message);
+    }
+
   }else{
     Serial.println("Erro in HTTP request");
   }
   delay(10);
+  http.end();
+}
+
+void Send_Message(String message){
+  String url = "https://api.callmebot.com/whatsapp.php?phone=" + PhoneNumber + "&apikey=" + ApiKey + "&text=" + urlEncode(message);
+  http.begin(url);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  int httResponseCode = http.POST(url);
+  
+  if (httResponseCode == 200){
+    Serial.println("Message sent successfully");
+  }else {
+    Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httResponseCode);
+  }
   http.end();
 }
 
@@ -53,5 +77,5 @@ void setup(){
 
 void loop(){
   Read_Price("BINANCE:BTCUSDT");
-  delay(1000);
+  delay(5000);
 }
